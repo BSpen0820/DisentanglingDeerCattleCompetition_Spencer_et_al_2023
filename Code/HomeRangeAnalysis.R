607cf0a6e449cbf1db8ea0c06770aed075e5be18
BSpen0820 <bdspencer1516@gmail.com>
2022-01-30 15:46:42 -0600

    Home Range Analysis Complete

---
+++ Code/HomeRangeAnalysis.R
@@ -0,0 +1,69 @@
+#Title: Home Range Analysis Presence vs Absence
+#By: Bryan Spencer
+#Date: 01/30/2022
+
+#######Read in Libraries###################
+
+library(tidyverse)
+library(raster)
+library(sf)
+library(sp)
+library(move)
+
+##########Home Ranges before and After###########
+
+Deer <- read.csv("./FinalAnalysisData/DeerGPSData.csv", header = T) #read in deer GPS data
+Deer$POSTime <- as.POSIXct(Deer$POSTime)
+Ind.ID <- unique(Deer$ID)
+Case <- unique(Deer$StStatus)
+
+rast <- raster("./FinalAnalysisData/PercentWoodyCover.tif")
+rast <- projectRaster(rast, crs = CRS('+proj=aeqd'))
+
+#Create an empty dataframe to store homerange data
+HR.table <- data.frame(matrix(nrow = 0, ncol = 4))
+names(HR.table) <- c("ID", "Status", "Type", "Area")
+
+#Dynamic Browning Bridge Loop
+for (x in 1:16) {
+  
+  ind.df <- Deer %>% filter(ID == Ind.ID[x]) #filter to an individual
+ 
+  for(i in 1:2){
+  
+    ind.df.st <- ind.df %>% filter(StStatus == Case[i])
+    
+    
+    M.Deer <- move(x = ind.df.st$LONGITUDE, y = ind.df.st$LATITUDE, 
+                   animal = ind.df.st$ID, time = ind.df.st$POSTime, data = ind.df.st, 
+                   proj= CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"), sensor = "GPS")
+    
+    M.Deer <- spTransform(M.Deer, CRSobj = "+proj=aeqd") #Transform GPS Data into AEQD projection
+    
+    #Create DBBMM for an individual deer
+    dbbmm <- brownian.bridge.dyn(object=M.Deer, location.error=10, margin=5,
+                                     window.size=21, raster= rast) 
+    
+    r2c <- raster2contour(dbbmm, levels = c(.95, .50))  # turns dbbmm object into contour lines
+    
+    IsoSF <- st_as_sf(r2c) #Converts the countor lines into an sf object
+    
+    IsoSTR <- st_cast(IsoSF, to = "MULTILINESTRING") #Converts contour lines into sf multilinestring
+    
+    IsoPoly <- st_cast(IsoSTR, to = "MULTIPOLYGON") #Converts multilinestring to MultiPolygon
+    
+    IsoPoly$Area <- as.numeric(abs(st_area(IsoPoly)/10000)) #Calculates the polygon area in hectares
+    
+    IsoPoly$ID <- ind.df.st$ID[x] #Copy the Deer ID
+    
+    IsoPoly$Type <- ifelse(IsoPoly$level == "0.5", "Core", "HR") #Add whether it the core area or homerange
+    
+    IsoPoly$Status <- unique(ind.df.st$StStatus) #Copy the stocking status
+    
+    Info <- st_drop_geometry(IsoPoly[,c(4,6,5,3)]) #Copy into format to add to empty HR table
+    
+    HR.table <- rbind(HR.table, Info) #Add the Id, Date, Isopleth level, and Area to HR table
+  }
+}
+
+write.csv(HR.table, "./FinalAnalysisOutput/HRData.csv", row.names = F)
